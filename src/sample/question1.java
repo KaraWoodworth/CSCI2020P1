a853c1d055d43ab6b65bbc856f468c1a2ba4c521
Kara <woodworth_cary@hotmail.com>
2020-03-08 11:24:27 -0400

    Completed requirements for question 1

--- src/sample/question1.java
+++ src/sample/question1.java
@@ -1,16 +1,42 @@
 package sample;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import java.util.Random;
 
 public class question1 extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World2");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Assignment 1 q1");
+
+        // basic window structure setup
+        Pane mpane = new Pane();
+        GridPane maingrid = new GridPane();
+
+        //setting up three imageview objects, path set by generateCardPath()
+        ImageView card1 = new ImageView( new Image(generateCardPath()) );
+        ImageView card2 = new ImageView( new Image(generateCardPath()) );
+        ImageView card3 = new ImageView( new Image(generateCardPath()) );
+
+        // configure layout of cards in the main grid
+        maingrid.add(card1,0,0);
+        maingrid.add(card2,1,0);
+        maingrid.add(card3,2,0);
+        maingrid.setHgap(6); // gap between cards
+        //add grid to Pane
+        mpane.getChildren().add(maingrid);
+
+        //3 cards width is 72 x 3 = 216, add 12 for column gap
+        primaryStage.setScene(new Scene(mpane, 228, 96));
         primaryStage.show();
+    }
+
+    public String generateCardPath(){
+        // create random number between 1 and 54, need to add 1 as default behavior is 0 to max
+        return "file:Cards/" + Integer.toString((int)(new Random().nextInt(54)+1)) + ".png";
     }
 }
