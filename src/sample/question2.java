9597b6460c505c9e8dc9813eb867948f8ba2834c
= <=>
2020-03-08 21:41:50 -0400

    Completed question 2 requirements

--- src/sample/question2.java
+++ src/sample/question2.java
@@ -1,6 +1,9 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +13,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+
 public class question2 extends Application {
 
     public static void main(String[] args) {
@@ -17,10 +21,6 @@ public class question2 extends Application {
     }
     // features that require interactivity
     public TextField[] m_fields;
-    public TextField m_amount; // initial investment amount
-    public TextField m_years; // years to calculate interest
-    public TextField m_interest; // annual interest rate
-    public TextField m_fval; // future value
 
     @Override
     public void start(Stage primaryStage) {
@@ -29,17 +29,7 @@ public class question2 extends Application {
         // setting up the scene
         GridPane mainGrid = new GridPane();
         GridPane inputGrid = new GridPane();
-        m_amount = new TextField();
-        m_years = new TextField();
-        m_interest = new TextField();
-        m_fval = new TextField();
 
-        m_interest.setAlignment(Pos.BOTTOM_RIGHT);
-        m_fval.setAlignment(Pos.BOTTOM_RIGHT);
-
-        m_fval.setEditable(false);
-        m_fval.setText("0");
-        m_fval.setStyle("-fx-control-inner-background: #dedede");
         Label[] labels = new Label[4];
         m_fields = new TextField[4];
         for(int i = 0; i < labels.length; i++) {
@@ -51,25 +41,52 @@ public class question2 extends Application {
             inputGrid.add(labels[i],0,i);
             inputGrid.add(m_fields[i],1,i);
         }
+        //set field specific attributes
         labels[0].setText("Investment Amount");
         labels[1].setText("Years");
         labels[2].setText("Annual Interest Rate");
         labels[3].setText("Future value");
-        Label label_fval = new Label("Years");
+        m_fields[3].setStyle("-fx-control-inner-background: #dedede");
 
-
-
-
-        Button calcButton = new Button();
+        Button calcButton = new Button("Calculate");
+        calcButton.setAlignment(Pos.BOTTOM_RIGHT);
+        calcButton.setOnAction(new CalculatePressed());
 
         inputGrid.setHgap(10);
         mainGrid.setVgap(20);
         mainGrid.setAlignment(Pos.BASELINE_RIGHT);
+        mainGrid.setHalignment(calcButton, HPos.RIGHT);
         mainGrid.add(inputGrid,0,0);
         mainGrid.add(calcButton,0,1);
 
         mpane.getChildren().add(mainGrid);
         primaryStage.setScene(new Scene(mpane, 400, 200));
         primaryStage.show();
+    }
+    class CalculatePressed implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            //need to ensure there is values in fields
+            // m_fields[0] = initial amount
+            // m_fields[1] = Years
+            // m_fields[2] = Annual Interest Rate
+            // m_fields[3] = future value that we are updating
+            //future value = InvestmentAmount * (1 + monthlyInterestRate)^years*12
+            Double InvestmentAmount = Double.parseDouble(m_fields[0].getText());
+            Double years = Double.parseDouble(m_fields[1].getText());
+            Double monthlyInterestRate = Double.parseDouble(m_fields[2].getText()) / 1200;
+            Double incAmount = monthlyInterestRate + 1.0;
+            //m_fields[2].setText(Double.toString(monthlyInterestRate));
+            //m_fields[1].setText(Double.toString(incAmount));
+            Double futureValue = incAmount;
+            for(int i = 0; i < (years*12) - 1;i++){
+                futureValue = futureValue * incAmount;
+                //System.out.print(futureValue + " ");
+            }
+            futureValue = futureValue * InvestmentAmount;
+            System.out.println (Math.round(futureValue*100) / 100);
+            System.out.print(Math.round(futureValue*100)/100);
+            m_fields[3].setText(String.format("%.2f", futureValue));
+        }
     }
 }
